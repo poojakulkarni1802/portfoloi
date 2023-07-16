51dd6e71e82862659d82698ca6e1ffcfdec15939
poojakulkarni1802 <139168561+poojakulkarni1802@users.noreply.github.com>
2023-07-16 18:33:52 +0530

    Add files via upload

--- connection.php
+++ connection.php
@@ -1,17 +1,23 @@
 <?php
-$name=$_POST['name'];
-$sug=$_POST['suggestion'];
-$conn=new mysqli('localhost','root','','query');
-if($conn->connect_error)
+$servername="localhost";
+$username="root";
+$password='';
+$dbase='query';
+$conn= mysqli_connect($servername,$username,$password,$dbase) or die("unable to connect");
+if(isset($_POST['submit']))
 {
-    die('Connection Failed:'.$conn->connect_error);
-}
-else{
-    $stm=$conn->prepare('insert the sugg(name,suggestion)
-    values(?,?,?)');
-    $stm->bind_param("hello",$name,$sug);
-    $stm->execute();
-    $stm->close();
-    $conn->close();
+    $name=$_POST['name'];
+    $suggest=$_POST['suggestion'];
+    $sql="insert into suggestion(full_name,suggestion) values('$name','$suggest')";
+    if(mysqli_query($conn,$sql))
+    {
+        echo"<script>alert('new record entered')</script>";
+        echo"<script>window.open('index.html','_self')</script>";
+    }
+    else
+    {
+        echo"error".mysqli_error($conn);
+    }
+    mysqli_close($conn);
 }
 ?>
